7fa841168879ec6269c030946ca9f7efb953db36
Takeshiyong <takeshiyong.@gmail.com>
2019-05-07 10:41:17 +0800

    search

--- src/components/city.vue
+++ src/components/city.vue
@@ -1,14 +1,17 @@
 <template>
     <div>
         <Header/>
+        <Search/>
     </div>
 </template>
 
 <script>
-import Header from './city/header.vue'
+import Header from './city/header.vue';
+import Search from './city/search.vue';
 export default {
     components: {
-        Header
+        Header,
+        Search
     }
 }
 </script>
---
+++ src/components/city/search.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="search">
+        <input type="text" name="" id="" placeholder="输入城市名或拼音" class="search-con">
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="less" scoped>
+@import "~styles/common.less";
+.search{
+    height: 0.72rem;
+    background: @bgcolor;
+    padding: .1rem .2rem;
+    overflow: hidden;
+    box-sizing: border-box;
+    .search-con{
+        height: 100%;
+        width: 100%;
+        border-radius: .1rem;
+        text-align: center;
+        color: #666;
+    }   
+}
+</style>
